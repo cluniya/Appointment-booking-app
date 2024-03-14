4e124c4c4af989496d055f6054d3010969bffcb4
cluniya <chandanluniya1986@gmail.com>
2024-03-14 13:42:11 +0530

    Correct Way to Store Data in Local Storage A13

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-function saveToLocalStorage(event){
-    event.preventDefault(event);
-    const Name = event.target.username.value;
-    const Email = event.target.email.value;
-    const Phone = event.target.phone.value;
-    localStorage.setItem('Username',Name);
-    localStorage.setItem('Email',Email);
-    localStorage.setItem('Phone',Phone);
-
-}
+function handleFormSubmit(event) {
+    event.preventDefault(); // Prevent the default form submission
+    
+    // Get form input values
+    let username = event.target.username.value;
+    let email = event.target.email.value;
+    let phone = event.target.phone.value;
+    
+    // Create user object
+    let user = {
+      username: username,
+      email: email,
+      phone: phone
+    };
+  
+    // Store user object in local storage
+    localStorage.setItem('User Details', JSON.stringify(user));
+  
+    // Optionally, you can provide feedback to the user
+    // alert('User details submitted successfully!');
+  }
+  
+  // Exporting the function for testing purposes
+  module.exports = handleFormSubmit;
+  
